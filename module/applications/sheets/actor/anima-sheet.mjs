857a915a48586de29ac49b9432f1890ee7e4f3a1
DFolks <aldwyn.wot@gmail.com>
2026-05-27 19:52:49 -0600

    feat: make sheet editable

--- module/applications/sheets/actor/anima-sheet.mjs
+++ module/applications/sheets/actor/anima-sheet.mjs
@@ -1,21 +1,30 @@
 export class ODAMAnimaSheet extends foundry.applications.api.HandlebarsApplicationMixin(
-  foundry.applications.sheets.ActorSheetV2
+  foundry.applications.sheets.ActorSheetV2,
 ) {
   static DEFAULT_OPTIONS = {
     classes: ["odam", "sheet", "actor", "anima"],
     position: {
       width: 720,
-      height: 800
+      height: 800,
     },
     window: {
       title: "ODAM.AnimaSheet.Title",
-      resizable: true
-    }
+      resizable: true,
+    },
+    form: {
+      handler: ODAMAnimaSheet.#onSubmitForm,
+      submitOnChange: true,
+      closeOnSubmit: false,
+    },
   };
 
   static PARTS = {
     form: {
-      template: "systems/odam/templates/actor/anima-sheet.hbs"
-    }
+      template: "systems/odam/templates/actor/anima-sheet.hbs",
+    },
   };
+
+  static async #onSubmitForm(event, form, formData) {
+    await this.document.update(formData.object);
+  }
 }
